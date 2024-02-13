a378b5c4ec8c32f93f90e5d79082019b91985599
Vishakha Vitthal Potdar <91433090+VishakhaPotdar@users.noreply.github.com>
2024-02-13 22:32:46 +0530

    Add files via upload

---
+++ practice13.c
@@ -0,0 +1,14 @@
+//Program to print 1 to 10 nos. 
+#include<stdio.h>
+
+int main()
+{
+    int No1, No2 = 0;
+    for(No1=1; No1<=10; No1++)
+    {
+        No2=No1;
+        printf("%d\n", No2);
+    }
+    
+    return 0;
+}
