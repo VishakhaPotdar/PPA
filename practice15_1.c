6ae3f607b25dae423f2045c5fb9f833e3a318799
Vishakha Vitthal Potdar <91433090+VishakhaPotdar@users.noreply.github.com>
2024-02-15 23:59:50 +0530

    Add files via upload

---
+++ practice15_1.c
@@ -0,0 +1,12 @@
+//Write a program to accept number from the user
+#include<stdio.h>
+
+int main()
+{
+    int No1, No2 = 0;
+    printf("Enter any digit\n");
+    scanf("%d", &No1);
+    
+    printf("Your entered number is : %d", No1);
+    return 0;
+}
